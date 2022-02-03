ec0ac5e3ffa09a43ffb4b560a654dcb8025f3eed
Mariana.Salez <mariana.i.salez@gmail.com>
2022-02-03 20:19:18 -0300

    Mejoras de codigo rev 1

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -9,20 +9,25 @@ import './cart.css'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import { addDoc, collection, getFirestore } from 'firebase/firestore'
+import Card from 'react-bootstrap/Card'
+import { ListGroupItem, ListGroup } from 'react-bootstrap'
 
 //ACA HAY QUE MEJORAR PARA QUE DIGA, IR A COMPRAR Y TU CARRITO ESTA VACIO. ADEMAS, AMPLIAR DETALLE PARA QUE ME HAGA LA SUMA
 
 export default function Cart() {
 
-    const{cartList, vaciarCarrito,valorCompra}=useContext(CartContext)
+    const{cartList, vaciarCarrito,valorCompra,ordenGenerada,setOrdengenerada}=useContext(CartContext)
+
+    
     const [dataForm , setDataForm ] = useState({
         email: '',
         name: '',
         phone: ''
     });
     const [idOrden, setIdOrden] = useState('');
+    
 
-
+//Funcion para generar orden de compra
     const realizarCompra=async(e)=>{
         e.preventDefault()
         let orden={}
@@ -44,9 +49,9 @@ export default function Cart() {
         const oredenCollection = collection(db, 'ordenes')
         await addDoc(oredenCollection, orden)
         .then(resp => setIdOrden(resp.id))
-        .then(() => console.log(idOrden))
         .catch(err => console.log(err))
-        .finally(()=>vaciarCarrito())
+        .then(() => console.log(idOrden))
+        .finally(()=>setOrdengenerada(true))
 
     }
 
@@ -57,100 +62,128 @@ export default function Cart() {
         })
     }
 
+   
 
+ 
 
     return (
         
         <div className='justify-content-center'>
-               {(cartList.length===0)? 
-                <div>
-                    <h1>Su Proxima compra lo esta esperando</h1>
-                        <Link  to ='/'>
-                            <Button variant="primary"> Ir a Comprar </Button>
-                        </Link>
-                </div>
-                :
-                <div>
+               {//Carrito vacio
+               (cartList.length===0)? 
+            
+                    <div>
+                        <h1>Su Proxima compra lo esta esperando</h1>
+                            <Link  to ='/'>
+                                <Button variant="primary"> Ir a Comprar </Button>
+                            </Link>
+                     </div>
+
+                //Carrito lleno y orden generada  
+                :ordenGenerada?
+                
+
+                <div >
+                    <Card style={{ width: '18rem' }} className='justify-content-center'>
+                        <Card.Img variant="top" src="https://res.cloudinary.com/dvkvyi1dr/image/upload/v1643930025/carritoCompra_Realizada_rnl8um.gif" />
+                        <Card.Body>
+                            <Card.Title className='text-success'>Gracias por su compra</Card.Title>
+                            <Card.Text className='text-dark'>
+                                El Identificador de su compra es el siguiente: 
+                          {idOrden}
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Body>
+                            <Link  to ='/'>
+                                <Button onClick={vaciarCarrito}> Nueva Compra </Button>
+                            </Link>
+                        </Card.Body>
+                    </Card>
+                
+        </div>
+               
+            :
+            <div>
                       
                             
-                    <h1>Detalle del Carrito</h1>
-                            
-                            
-                         
-                    <Placeholder/>
-
+            <h1>Detalle del Carrito</h1>
                     
-                    <Row>
-                        <Col  xs={7}>
-                        <Table className='table' responsive="sm">
-                                <thead>
-                                    <tr>
-                                        <th>Nombre</th>
-                                        <th>Autor</th>
-                                        <th>precio</th>
-                                        <th>cantidad</th>
-                                        <th>subtotal</th>  
-                                    </tr>
-                                </thead>
-                                <tbody className='justify-content-center'>
-                                    {cartList.map(prod=>
-                                        <CartItem key={prod.id} item={prod}/>)}
-                                </tbody>
-                                
-                                    
-                                    </Table>
-                                    <Row>
-                                        <h4>El total de su compra es ${valorCompra}</h4>
-                                    </Row>
-                        </Col>
-                        
-                        <Col>
-                        <Container className='formContainer' >
-
-                            <Form  onSubmit={realizarCompra} >
-
-                            <Form.Group className="mb-3">
-                                <Form.Label>Usuario</Form.Label>
-                                <Form.Control type="text" required="required" name='name'  placeholder="Ingrese su nombre" onChange={handleChange}
-                                value={dataForm.name}/>
-                            </Form.Group>
-
-                            <Form.Group className="mb-3">
-                                <Form.Label>Telefono</Form.Label>
-                                <Form.Control type="number" required="required"  name='phone' placeholder='tel' onChange={handleChange} value={dataForm.phone}/>
-                            </Form.Group>
-
-
-                                
-                                <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" required="required" name='email' placeholder='email' onChange={handleChange}value={dataForm.email} />
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" required="required" label="Acepto terminos y condiciones de Librate" />
-                            </Form.Group>
-                            <Button variant="primary" type="submit">
-                            Finalizar compra</Button>
-
-                            </Form>
-                        </Container>
-                        
-                        </Col>
-                    </Row>
-
-                    <Placeholder/>
-                       
-                                  
-
-                        <Row>
-                            
-                        <Col><Button  onClick={vaciarCarrito}> Vaciar Carrito</Button>
-                            </Col>
-                            
-                        </Row>
                     
-                    </div>
+                 
+            <Placeholder/>
+
+            
+            <Row>
+                <Col  xs={7}>
+                <Table className='table' responsive="sm">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Autor</th>
+                                <th>precio</th>
+                                <th>cantidad</th>
+                                <th>subtotal</th>  
+                            </tr>
+                        </thead>
+                        <tbody className='justify-content-center'>
+                            {cartList.map(prod=>
+                                <CartItem key={prod.id} item={prod}/>)}
+                        </tbody>
+                        
+                            
+                            </Table>
+                            <Row>
+                                <h4>El total de su compra es ${valorCompra}</h4>
+                            </Row>
+                </Col>
+                
+                <Col>
+                <Container className='formContainer' >
+
+                    <Form  onSubmit={realizarCompra} >
+
+                    <Form.Group className="mb-3">
+                        <Form.Label>Usuario</Form.Label>
+                        <Form.Control type="text" required="required" name='name'  placeholder="Ingrese su nombre" onChange={handleChange}
+                        value={dataForm.name}/>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3">
+                        <Form.Label>Telefono</Form.Label>
+                        <Form.Control type="number" required="required"  name='phone' placeholder='tel' onChange={handleChange} value={dataForm.phone}/>
+                    </Form.Group>
+
+
+                        
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" required="required" name='email' placeholder='email' onChange={handleChange}value={dataForm.email} />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" required="required" label="Acepto terminos y condiciones de Librate" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                    Finalizar compra</Button>
+
+                    </Form>
+                </Container>
+                
+                </Col>
+            </Row>
+
+            <Placeholder/>
+               
+                          
+
+                <Row>
+                    
+                <Col><Button  onClick={vaciarCarrito}> Vaciar Carrito</Button>
+                    </Col>
+                    
+                </Row>
+            
+            </div>
 
 
 
--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -4,13 +4,10 @@ import { useContext } from 'react'
 import {createContext,useState} from 'react'
 
 
-
-
 export function useCartContext() {
     return useContext(CartContext)
     
 }
-
 
 export const CartContext= createContext([])
 
@@ -21,53 +18,63 @@ export const CartContextProvider=({children})=>{
     const[cartList,setCartList]=useState([])
     const[cartNumber,setCartNumber]=useState(0)
     const[valorCompra, setValorCompra]=useState(0)
+    const[ordenGenerada, setOrdengenerada]=useState(false)
 
 
   
-
+//Funcion para agregar elementos
     function agregarCarrito(items){
         const index=cartList.findIndex(i=>i.id===items.id)
+
+        //si elemento existe
         if (index > -1) {
-            
             const cantidadVieja=cartList[index].cantidad
-            cartList.splice(index,1)
-            setCartList([...cartList,{...items,cantidad:items.cantidad+cantidadVieja}])
-         
+            setCartList([cartList.filter(element => element !== items),{...items,cantidad:items.cantidad+cantidadVieja}])  
         }
+        //Si el elemento no existe en el carrito
         else{
             setCartList([...cartList,items])
             
         }
+
+        //Seteamos cantidad en el CartWidget
         setCartNumber(cartNumber+(items.cantidad))
+
+        //Calculo y seteo de valores
         const subtotal=((items.cantidad)*(items.precio))
         const nuevoValorCompra=(valorCompra+subtotal)
         setValorCompra(nuevoValorCompra)
-        console.log('context',valorCompra)
     }
 
+
+
+//Funcion para vaciar todo el carrito
     function vaciarCarrito(){
         setCartList([])
         setCartNumber(0)
         setValorCompra(0)
-    }
-    
-    function eliminarItem(items) {
-        const index=cartList.findIndex(i=>i.id===items.id)
-        const cartnvo=cartNumber-items.cantidad
-        const subtotal=(items.cantidad*items.precio)
-        cartList.splice(index,1)
-        setCartList([...cartList])
-        setCartNumber(cartnvo)
-        setValorCompra(valorCompra-subtotal)
-        console.log(valorCompra)
-        
+        setOrdengenerada(false)
     }
 
-   
-  
+
+//Funcion para eliminar un solo elemento del carrito
+
+    function eliminarItem(items) {
+        //Seteo nueva lista
+        setCartList(cartList.filter(element => element !== items))
+       
+        //Seteo cantidad CartWidget
+        const cartnvo=cartNumber-items.cantidad
+        setCartNumber(cartnvo)
+
+        //Seteo calculo de total
+        const subtotal=(items.cantidad*items.precio)
+        setValorCompra(valorCompra-subtotal)
+    }
+
 
     return(
-        <CartContext.Provider value={{cartList, agregarCarrito, vaciarCarrito,cartNumber, eliminarItem,valorCompra}}>
+        <CartContext.Provider value={{cartList, agregarCarrito, vaciarCarrito,cartNumber, eliminarItem,valorCompra,ordenGenerada,setOrdengenerada}}>
             {children}
         </CartContext.Provider>
     )
